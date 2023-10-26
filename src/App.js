8f3c28f1669c92a0fbc31be8d22bf5d1b57952e0
A-L-O-K <apvhu03@gmail.com>
2023-10-27 00:25:10 +0530

    Changed AudioRecorder

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       {/* <Signup /> */}
       {/* <Login /> */}
-      {/* <AudioRecorder /> */}
+      <AudioRecorder />
       {/* <SendAudio /> */}
     </div>
   );
--- src/components/AudioRecorder/AudioRecorder.jsx
+++ src/components/AudioRecorder/AudioRecorder.jsx
@@ -3,12 +3,7 @@ import './styles.css';
 
 const AudioRecorder = () => {
   const [isRecording, setIsRecording] = useState(false);
-  const [audioData, setAudioData] = useState(null);
   const mediaRecorder = useRef(null);
-  const audioContext = useRef(null);
-  const analyser = useRef(null);
-  const canvasRef = useRef(null);
-  const requestRef = useRef(null);
   const recordedChunks = useRef([]);
   const recordImg = "https://cdn-icons-png.flaticon.com/512/60/60955.png";
   const stopImg = "https://cdn-icons-png.flaticon.com/512/1082/1082810.png";
@@ -35,13 +30,10 @@ const AudioRecorder = () => {
           a.click();
           window.URL.revokeObjectURL(url);
           recordedChunks.current = [];
-          setIsRecording(false);
-          cancelAnimationFrame(requestRef.current);
         };
 
         mediaRecorder.current.start();
         setIsRecording(true);
-        visualize(stream);
       } catch (err) {
         console.error('Error accessing the microphone:', err);
       }
@@ -53,54 +45,15 @@ const AudioRecorder = () => {
     }
   };
 
-  const visualize = (stream) => {
-    audioContext.current = new (window.AudioContext || window.webkitAudioContext)();
-    const source = audioContext.current.createMediaStreamSource(stream);
-    analyser.current = audioContext.current.createAnalyser();
-    analyser.current.fftSize = 2048;
-    const bufferLength = analyser.current.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
-
-    const canvasCtx = canvasRef.current.getContext('2d');
-    canvasCtx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-
-    const draw = () => {
-      requestRef.current = requestAnimationFrame(draw);
-      analyser.current.getByteTimeDomainData(dataArray);
-      canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-      canvasCtx.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-      canvasCtx.lineWidth = 2;
-      canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
-      canvasCtx.beginPath();
-
-      const sliceWidth = (canvasRef.current.width * 1.0) / bufferLength;
-      let x = 0;
-      for (let i = 0; i < bufferLength; i++) {
-        const v = dataArray[i] / 128.0;
-        const y = (v * canvasRef.current.height) / 2;
-        if (i === 0) {
-          canvasCtx.moveTo(x, y);
-        } else {
-          canvasCtx.lineTo(x, y);
-        }
-        x += sliceWidth;
-      }
-      canvasCtx.lineTo(canvasRef.current.width, canvasRef.current.height / 2);
-      canvasCtx.stroke();
-    };
-    draw();
-  };
-
   return (
     <div className="record-container">
-      <canvas ref={canvasRef} width="200" height="100"></canvas>
       <div className="record-button">
         <button onClick={handleRecording}>
           <img src={isRecording ? stopImg : recordImg} alt={isRecording ? 'Stop' : 'Record'} />
         </button>
       </div>
     </div>
-  );
+  );  
 };
 
 export default AudioRecorder;
