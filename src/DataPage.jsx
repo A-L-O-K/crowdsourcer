9d88e287c9a20e303fb7a070ea753d37f4e532eb
Gaadha VS <111566224+gaadhavs@users.noreply.github.com>
2023-11-18 11:55:51 +0530

    DataPage.jsx

--- src/DataPage.jsx
+++ src/DataPage.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
+import Header from './components/Common/Header';
+
+
 
 const Data = () => {
   const [recordedVoices, setRecordedVoices] = useState([]);
   const [currentVoiceIndex, setCurrentVoiceIndex] = useState(0);
   const [currentRecording, setCurrentRecording] = useState('');
+
+  const hindiAksharas = ['अ', 'आ', 'इ', 'ई']; // Add more Hindi aksharas as needed
 
   const handlePreviousVoice = () => {
     if (currentVoiceIndex > 0) {
@@ -17,35 +22,35 @@ const Data = () => {
   };
 
   const handleNextWord = () => {
-    // Implement logic to fetch the next word to record
-    // For now, let's assume you have an array of words to record
-    const wordsToRecord = ['Word1', 'Word2', 'Word3'];
-    
     // Assuming you've recorded the current word
     setRecordedVoices([...recordedVoices, currentRecording]);
 
-    // Move to the next word
+    // Move to the next word (Hindi akshara)
     const nextIndex = currentVoiceIndex + 1;
     setCurrentVoiceIndex(nextIndex);
-    setCurrentRecording(wordsToRecord[nextIndex] || '');
+    setCurrentRecording(hindiAksharas[nextIndex] || '');
   };
 
   return (
-    <div style={styles.dataBox}>
-      <h1>Data</h1>
-      <div>
-        <button onClick={handlePreviousVoice}>Previous Voice</button>
-        <button onClick={handleRetryVoice}>Retry Voice</button>
-        <button onClick={handleNextWord}>Next Word to Record</button>
-      </div>
+    <>
+    <Header />
+    <div className="flex flex-row gap-5">
+    <div className="px-4 py-8 border-2">
+      <h1>Data...</h1>
+      
       <div style={styles.recordingArea}>
-        <textarea
-          value={currentRecording}
-          onChange={(e) => setCurrentRecording(e.target.value)}
-          placeholder="Record your voice here..."
-          rows="4"
-          cols="50"
-        />
+        {hindiAksharas.map((akshara, index) => (
+          <div key={index}  className="flex flex-col">
+            <div className="flex gap-4 border-4 p-2 shadow-md  text-md transition ease-in-out hover:bg-blue-500">
+              {/* Add your play button icon here */}
+              <span role="img" aria-label="play-button">
+                ▶
+              </span>
+              <p>Voice Name</p>
+            </div>
+            
+          </div>
+        ))}
       </div>
       <div>
         <p>Recorded Voices:</p>
@@ -55,7 +60,23 @@ const Data = () => {
           ))}
         </ul>
       </div>
-    </div>
+      </div>
+
+      <div className=" flex flex-col justify-evenky gap-2 border-4 p-48">
+        <p>Text Content</p>
+        <div className="flex gap-2">
+        <div className="flex gap-4 border-4 p-2 shadow-md  text-md transition ease-in-out hover:bg-blue-500">
+          <button className="border-2 border-black p-4">Prev</button> </div>
+          <div className="flex gap-4 border-4 p-2 shadow-md  text-md transition ease-in-out hover:bg-blue-500">
+          <button   className="border-2 border-black p-4">Retry</button></div>
+          <div className="flex gap-4 border-4 p-2 shadow-md  text-md transition ease-in-out hover:bg-blue-500">
+          <button  className="border-2 border-black p-4">Next</button></div>
+
+        </div>
+        </div>
+      </div>
+    
+    </>
   );
 };
 
@@ -70,6 +91,19 @@ const styles = {
   recordingArea: {
     marginTop: '20px',
   },
+  textboxWithIcons: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  iconGroup: {
+    marginRight: '10px',
+    fontSize: '1.5em',
+  },
+  formGroup: {
+    marginBottom: '15px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 };
-
 export default Data;
